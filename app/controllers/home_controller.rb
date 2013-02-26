940fe4d64bf4bfae5d5708ef3c394224a01ffacc
Juan F. Pérez <j.guitar@gmail.com>
2013-02-26 17:25:24 +0100

    playing with flash messages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
-
+    flash[:success] = 'Éxito'
+    flash[:error] = 'Error'
+    flash[:alert] = 'Atención'
+    flash[:notice] = 'Hey'
   end
 end
